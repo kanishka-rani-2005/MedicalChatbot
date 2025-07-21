e94a91d916b33cfbc095322b538fbac3670f4b41
Kanishka Rani <kanishka22043@gmail.com>
2025-07-21 23:49:02 +0530

    update requirements

--- store_index.py
+++ store_index.py
@@ -1,25 +1,25 @@
+# ! pip install langchain ctransformers langchain-community pinecone pypdf sentence-transformers
 from src.helper import load_pdf,text_split,download_embedding
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 import os
 from dotenv import load_dotenv
-
+from src.logger import logger
 import pinecone
 
 load_dotenv()
 
-extract_data=load_pdf('data/')
 
+extract_data=load_pdf('data/')
+logger.info('Load data succesfully')
 
 text_chunks=text_split(extract_data=extract_data)
-
+logger.info('split data successfully')
 embeddings=download_embedding()
 
-
+logger.info('Data embedding done')
 
 PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 
 pc = Pinecone(api_key=PINECONE_API_KEY)
 index_name = "medicalchatbot"
@@ -32,5 +32,8 @@ docsearch = PineconeVectorStore.from_texts(
     pinecone_api_key=PINECONE_API_KEY
 )
 
+logger.info('Vector transfer done')
 
 
+
+print(docsearch)
