165477bb715d07460b95e602ba1abc642e3a1f84
Kanishka Rani <kanishka22043@gmail.com>
2025-07-21 18:01:04 +0530

    Update setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Read the contents of requirements.txt
+with open('requirements.txt') as f:
+    requirements = [
+        line.strip() for line in f
+        if line.strip() and not line.startswith('-e') and not line.startswith('#')
+    ]
+
+    
+
+setup(
+    name='Medical Chatbot',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    description='Your project description here',
+    author='Kanishka Rani',
+    author_email='kanishka22043@gmail.com',
+    url='https://github.com/kanishka-rani-2005/MedicalChatbot/tree/main',
+    python_requires='>=3.6',
+)
