4ff3a7ae99b07e5142f4620f9779ce8848e6637a
Kanishka Rani <kanishka22043@gmail.com>
2026-06-15 13:06:34 +0530

    fix bug

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,6 @@
 
 from langchain_community.document_loaders import DirectoryLoader,PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters  import RecursiveCharacterTextSplitter
 from langchain_huggingface  import HuggingFaceEmbeddings
 
 
