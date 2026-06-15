d2516b7ed6170eccf34f55cd602f437827fd42a1
Kanishka Rani <kanishka22043@gmail.com>
2026-06-15 14:23:58 +0530

    UPDATE

--- app.py
+++ app.py
@@ -1,76 +1,75 @@
 from flask import Flask, render_template, request
 from src.helper import download_embedding
-from langchain_pinecone import PineconeVectorStore
-from pinecone import Pinecone
+
+from langchain_community.vectorstores import Pinecone
 from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
+from langchain_community.llms import CTransformers
+
 from dotenv import load_dotenv
 from src.prompt import *
+
+import pinecone
 import os
 
 app = Flask(__name__)
 
 load_dotenv()
 
-PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
 
 embeddings = download_embedding()
 
+# Initialize Pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_ENVIRONMENT
+)
 
-pc = Pinecone(api_key=PINECONE_API_KEY)
 index_name = "medical-chatbot"
-index = pc.Index(index_name)
 
-#Loading the index
-docsearch=PineconeVectorStore.from_existing_index(index_name,embeddings)
+# Load existing Pinecone index
+docsearch = Pinecone.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
 
-
-prompt=PromptTemplate(
-    input_variables=['context','question'],
+prompt = PromptTemplate(
+    input_variables=["context", "question"],
     template=prompt_template
 )
 
-chain_type_kwargs={'prompt':prompt}
+chain_type_kwargs = {"prompt": prompt}
 
-
-llm=CTransformers(
-    model=r'model\llama-2-7b-chat.ggmlv3.q4_0.bin',
-    model_type='llama',
+llm = CTransformers(
+    model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+    model_type="llama",
     config={
-        'max_new_tokens':512,
-        'temperature':0.8
+        "max_new_tokens": 512,
+        "temperature": 0.8
     }
 )
 
-qa=RetrievalQA.from_chain_type(
+qa = RetrievalQA.from_chain_type(
     llm=llm,
-    chain_type='stuff',
-    retriever=docsearch.as_retriever(search_kwargs={'k':2}),
+    chain_type="stuff",
+    retriever=docsearch.as_retriever(search_kwargs={"k": 2}),
     return_source_documents=True,
     chain_type_kwargs=chain_type_kwargs
 )
 
-
 @app.route("/")
 def index():
-    return render_template('index.html')
-
-
+    return render_template("index.html")
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
     msg = request.form["msg"]
-    input = msg
-    print(input)
-    result=qa({"query": input})
-    print("Query :",input)
-    print("Response : ", result["result"])
+
+    result = qa({"query": msg})
+
     return str(result["result"])
 
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= False)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=False)
--- store_index.py
+++ store_index.py
@@ -1,38 +1,37 @@
-# ! pip install langchain ctransformers langchain-community pinecone pypdf sentence-transformers
-from src.helper import load_pdf,text_split,download_embedding
-from langchain_pinecone import PineconeVectorStore
-from pinecone import Pinecone
-import os
+from src.helper import load_pdf, text_split, download_embedding
+from langchain_community.vectorstores import Pinecone
 from dotenv import load_dotenv
 from src.logger import logger
+import os
+import pinecone
 
 load_dotenv()
 
+extract_data = load_pdf('data/')
+logger.info('Load data successfully')
 
-extract_data=load_pdf('data/')
-logger.info('Load data succesfully')
+text_chunks = text_split(extract_data=extract_data)
+logger.info('Split data successfully')
 
-text_chunks=text_split(extract_data=extract_data)
-logger.info('split data successfully')
-embeddings=download_embedding()
-
+embeddings = download_embedding()
 logger.info('Data embedding done')
 
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
 
-pc = Pinecone(api_key=PINECONE_API_KEY)
+pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_ENVIRONMENT
+)
+
 index_name = "medical-chatbot"
-index = pc.Index(index_name)
 
-docsearch = PineconeVectorStore.from_texts(
+docsearch = Pinecone.from_texts(
     texts=[t.page_content for t in text_chunks],
     embedding=embeddings,
-    index_name=index_name,
-    pinecone_api_key=PINECONE_API_KEY
+    index_name=index_name
 )
 
 logger.info('Vector transfer done')
-
-
 
 print(docsearch)
